8860b206cf629dc93a8113cf33650986f0bb2e70
Micaela Marciukaitis <micaelamarciukaitis@gmail.com>
2024-12-25 22:55:39 -0300

    Agregando foto

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -3,7 +3,7 @@ import { about } from '../../portfolio'
 import './About.css'
 
 const About = () => {
-  const { name, role, description, resume } = about
+  const { name, role, description,  } = about
 
   return (
     <div className='about center'>
@@ -12,11 +12,17 @@ const About = () => {
           Hola, soy <span className='about__name'>{name}.</span>
         </h1>
       )}
+       <div className='foto'>
+       <img src='/fotoperfil.png' alt='Foto de Micaela' className='foto__img' />
+     </div>
 
       {role && <h2 className='about__role'> {role}.</h2>}
+     
       <p className='about__desc'>{description && description}</p>
 
-      <div className='about__contact center'>
+
+      
+      {/* <div className='about__contact center'>
         {resume && (
            <a href={resume} target='_blank' rel='noopener noreferrer'>
             <span type='button' className='btn btn--outline'>
@@ -24,7 +30,7 @@ const About = () => {
             </span>
           </a>
         )}
-      </div>
+      </div> */}
     </div>
   )
 }
